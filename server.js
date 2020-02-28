b6011b3573e6d1e59b2fc3ceaf68aebb48c78124
Ch3relle <bose.ann@gmail.com>
2020-02-28 12:03:01 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log('hey');
+
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3000, listening);
+
+function listening() {
+  console.log("listening...");
+}
+
+app.use(express).static('');
