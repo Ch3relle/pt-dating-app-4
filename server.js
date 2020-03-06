38fad4971aaff75af7037915e094bd1497203ddd
Ch3relle <bose.ann@gmail.com>
2020-03-06 12:19:28 +0100

    assignment week 1 backend

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-console.log('hey');
 
+// requiring express
 const express = require('express');
-
 const app = express();
 
-const server = app.listen(3000, listening);
+app.use(express.static('static'));
 
-function listening() {
-  console.log("listening...");
-}
+// '=>' means function
+app.get('/', (req, res) => {
+  res.send('Hello World') // can be found inside of the browser
+});
 
-app.use(express).static('');
+// about page
+app.get('/about', (req, res) => {
+  res.send([1,2,3]);
+});
+
+// contact page
+app.get('/contact', (req, res) => {
+  res.send('Contact here!');
+});
+
+app.get('/images/cat.jpg', (req, res) => {
+  res.send('images/cat.jpg');
+});
+
+// 404 page
+app.get('/404', (req, res) => {
+  res.send('404 Not Found');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
