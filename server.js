6ac2aa6bf5c6c35f5924ae72d4cb12c33e738c31
Ch3relle <bose.ann@gmail.com>
2020-03-12 14:23:23 +0100

    werken aan week 2 be
    een map aangemaakt voor html-pagina's (pages), server.js aangepast (laat nu html zien ipv berichten)

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 const express = require('express');
 const app = express();
 
+
+
 app.use(express.static('static'));
+
 
 // '=>' means function
 app.get('/', (req, res) => {
@@ -11,22 +14,23 @@ app.get('/', (req, res) => {
 });
 
 // about page
-app.get('/about', (req, res) => {
-  res.send([1,2,3]);
+app.get('/pages/about.html', (req, res) => {
+  res.send('/pages/about.html');
 });
 
 // contact page
-app.get('/contact', (req, res) => {
-  res.send('Contact here!');
+app.get('/pages/contact.html', (req, res) => {
+  res.send('/pages/contact.html');
 });
 
 app.get('/images/cat.jpg', (req, res) => {
   res.send('images/cat.jpg');
 });
 
-// 404 page
-app.get('/404', (req, res) => {
-  res.send('404 Not Found');
+// 404 page, user will be sent to a html-page
+app.get('/pages/404.html', (req, res) => {
+  res.send('pages/404.html');
 });
+
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
